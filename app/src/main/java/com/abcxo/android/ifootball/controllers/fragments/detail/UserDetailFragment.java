b9ed22b524b552ea4e2a2c2f342cd67882ed8d9d
神鵰者也！ <xiedao.xd@alibaba-inc.com>
2015-11-09 23:27:00 +0800

    aa

--- app/src/main/java/com/abcxo/android/ifootball/controllers/fragments/detail/UserDetailFragment.java
+++ app/src/main/java/com/abcxo/android/ifootball/controllers/fragments/detail/UserDetailFragment.java
@@ -1,18 +1,27 @@
 package com.abcxo.android.ifootball.controllers.fragments.detail;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.abcxo.android.ifootball.R;
+import com.abcxo.android.ifootball.constants.Constants;
+import com.abcxo.android.ifootball.databinding.FragmentDetailTweetBinding;
+import com.abcxo.android.ifootball.databinding.FragmentDetailUserBinding;
+import com.abcxo.android.ifootball.models.Tweet;
+import com.abcxo.android.ifootball.models.User;
 
 /**
  * Created by shadow on 15/11/4.
  */
 public class UserDetailFragment extends Fragment{
+    private User user;
     public static UserDetailFragment newInstance() {
         return newInstance(null);
     }
@@ -21,6 +30,15 @@ public class UserDetailFragment extends Fragment{
         UserDetailFragment fragment = new UserDetailFragment();
         if (args != null) fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            user = (User) getArguments().get(Constants.KEY_USER);
+        }
     }
 
     @Nullable
@@ -32,5 +50,20 @@ public class UserDetailFragment extends Fragment{
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+
+        FragmentDetailUserBinding binding = DataBindingUtil.bind(view);
+        binding.setUser(user);
     }
 }
